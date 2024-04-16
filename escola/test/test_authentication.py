59fff9eeec1042fdd6235c01dee6646c5c030972
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-04-16 20:45:04 -0300

    tests autenticação

--- escola/test/test_authentication.py
+++ escola/test/test_authentication.py
@@ -13,23 +13,9 @@ class AuthenticationUserTestCase(APITestCase):
     def test_autenticacao_user_com_credenciais_corretas(self):
         """Teste que verifica a autenticação de um user com as credenciais corretas"""
         user = authenticate(username='lais', password='lais')
-        self.assertTrue((user is not None) and user.is_authenticated,"A autenticação falhou.")
-        # self.assertFalse((user is not None) and user.is_authenticated)
+        self.assertTrue((user is not None) and user.is_authenticated)
 
-    def test_requisicao_get_nao_autorizada(self):
-        """Teste que verifica uma requisição GET não autorizada (sem autenticar)"""
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        # self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # def test_requisicao_get_com_user_autenticado(self):
-    #     """Teste que verifica uma requisição GET de um user autenticado"""
-    #     self.client.force_authenticate(self.user)
-    #     response = self.client.get(self.url)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-
-    # CONFIRMAR QUE NÃO ESTÁ AUTENTICADO
+     # CONFIRMAR QUE NÃO ESTÁ AUTENTICADO
     def test_autenticacao_de_user_com_username_incorreto(self):
         """Teste que verifica autenticação de um user com username incorreto"""
         user = authenticate(username='las', password='lais')
@@ -39,4 +25,20 @@ class AuthenticationUserTestCase(APITestCase):
         """Teste que verifica autenticação de um user com password incorreto"""
         user = authenticate(username='lais', password='las')
         self.assertFalse((user is not None) and user.is_authenticated)
+
+    def test_requisicao_get_nao_autorizada(self):
+        """Teste que verifica uma requisição GET não autorizada (sem autenticar)"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # fazer esse e puxar para o teste de estudante
+    def test_requisicao_get_com_user_autenticado(self):
+        """Teste que verifica uma requisição GET de um user autenticado"""
+        self.client.force_authenticate(self.user)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+   
 
